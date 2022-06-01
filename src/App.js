02ee0f914c5253c39e76090cad912dfbd3cbdc12
ozngrsc <ozangursucu@hotmail.com>
2022-06-01 10:16:40 +0300

    first page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>spotify clone</h1>
+    <div className="app">
+      <Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <a href="">LOGIN WITH SPOTIFY</a>
+    </div>
+  );
+}
+
+export default Login;
