7d80ba11fde6b635783a626e1e336e6c7c22f89f
ozngrsc <ozangursucu@hotmail.com>
2022-06-03 10:55:05 +0300

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,42 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import { Grid, Slider } from "@mui/material";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>footer</h1>
+      <div className="footer__left">
+        <p>Album and song details</p>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon className="footer__icon" fontSize="large" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
