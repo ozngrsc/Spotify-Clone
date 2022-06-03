bc08927cd4202c4435a7d05c2b9107826eb9c4a9
ozngrsc <ozangursucu@hotmail.com>
2022-06-03 15:59:17 +0300

    body icons

--- src/Body.js
+++ src/Body.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./Body.css";
 import Header from "./Header";
 import { useDataLayerValue } from "./DataLayer";
+import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
@@ -16,6 +19,14 @@ function Body({ spotify }) {
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
         </div>
       </div>
     </div>
